4ae1514bbcdd9a3cea864b16ed158f15738e010a
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-01 23:49:51 +0800

    two api calls done

--- route.js
+++ route.js
@@ -1,3 +1,25 @@
-const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+const axios = require('axios');
 
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+const URL = 'https://maps.googleapis.com/maps/api/directions/json';
+
+async function makeGoogleMapsAPICall(origin, destination, mode) {
+
+    await axios.get(URL, {
+        params: {
+            origin,
+            destination,
+            mode,
+            API,
+            key: GOOGLE_MAPS_API_KEY,
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    })
+}
+
+module.exports = {
+    makeGoogleMapsAPICall
+}
 
--- weather.js
+++ weather.js
@@ -1,1 +1,24 @@
+const axios = require('axios');
+
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
+const URL = 'https://api.openweathermap.org/data/2.5/onecall';
+
+
+async function makeOpenWeatherAPICall(lat, lon, exclude, appid) {
+
+    await axios.get(URL, {
+        params: {
+            lat,
+            lon,
+            exclude,
+            appid: OPEN_WEATHER_API_KEY,
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    })
+}
+
+module.exports = {
+    makeOpenWeatherAPICall
+}
