c922c206ee883a5004bab4a3432afefe0bdd28f4
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-03 00:55:04 +0800

    trying oyt distance api

--- route.js
+++ route.js
@@ -1,18 +1,19 @@
 const axios = require('axios');
 
 // https://developers.google.com/maps/documentation/distance-matrix/overview#Introduction
+// Correction: Wanted to use Legs feature of directions API 
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-const URL = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+const URL = 'https://maps.googleapis.com/maps/api/directions/json';
 
 
-// https://stackoverflow.com/questions/32994634/this-api-project-is-not-authorized-to-use-this-api-please-ensure-that-this-api
-async function makeGoogleMapsAPICall(origins, destinations, mode) {
+// In order to use the API, you need to enable the key: https://stackoverflow.com/questions/32994634/this-api-project-is-not-authorized-to-use-this-api-please-ensure-that-this-api
+async function makeGoogleMapsAPICall(origin, destination, travelMode) {
 
     const response = await axios.get(URL, {
         params: {
-            origins,
-            destinations,
-            mode,
+            origin,
+            destination,
+            travelMode,
             key: GOOGLE_MAPS_API_KEY,
         }
     })
