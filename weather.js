1e66746c892ed517a098d4246e5c68c49c1dd7fd
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-03 01:13:52 +0800

    fine tune weather api

--- weather.js
+++ weather.js
@@ -2,11 +2,12 @@ const axios = require('axios');
 
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 const URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const exclude = 'minutely,hourly,daily,alerts';
 
 
-async function makeOpenWeatherAPICall(lat, lon, exclude, appid) {
+async function makeOpenWeatherAPICall(lat, lon) {
 
-    await axios.get(URL, {
+    const response = await axios.get(URL, {
         params: {
             lat,
             lon,
@@ -14,9 +15,9 @@ async function makeOpenWeatherAPICall(lat, lon, exclude, appid) {
             appid: OPEN_WEATHER_API_KEY,
         }
     })
-    .then((response) => {
-        console.log(response);
-    })
+    .then(response => response.data)
+
+    return response;
 }
 
 module.exports = {
