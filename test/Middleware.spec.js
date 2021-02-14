f6caa996e7788b682cfa34fed6bb3f55a74bb6ce
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-14 20:20:23 +0800

    added some invalid values checks

--- test/Middleware.spec.js
+++ test/Middleware.spec.js
@@ -2,32 +2,61 @@ const Chai = require('chai');
 const ChaiAsPromised = require('chai-as-promised');
 const ChaiHttp = require('chai-http');
 const Sinon = require('sinon');
-const WeatherOverSpaceAndTime = require('../app');
 const { startServer } = require('../initializer');
-const Router = require('../router');
-const { errorHandling } = require('../error-handling');
-
 
 Chai.use(ChaiAsPromised);
 Chai.use(ChaiHttp);
-const { expect, should } = Chai;
 Chai.should();
 
-const testPortNumber = 8080;
+const testUrl = 'http://localhost:';
 
 describe("Router Tests", () => {
     describe("GET /route-weather", () => {
+        const testPortNumber = 8080;
         const fullRequest = '/route-weather?start=Brooklyn&end=Boston&mode=bicycling&increment=5';
         startServer(testPortNumber);
-        // const instance = new WeatherOverSpaceAndTime(Router, errorHandling, 8080);
         it('should respond with a 200', (done) => {
-            Chai.request(`http://localhost:` + testPortNumber)
+            Chai.request(testUrl + testPortNumber)
                 .get(fullRequest)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     done();
                 })
+        })
+    })
+})
+
+describe("Middleware tests", () => {
+    describe("checkQueryParams", () => {
+        const testPortNumber = 8081
+        const incompleteRequest = '/route-weather?';
+        startServer(testPortNumber);
+        it ('should reject with a 406 with all the missing keys', (done) => {
+            Chai.request(testUrl + testPortNumber)
+            .get(incompleteRequest)
+            .end((err, res) => {
+                res.should.have.status(406);
+                res.text.should.eql('Missing keys in start, end, mode, increment');
+                done();
+            })
+        })
+    })
+
+    describe('Check invalid values', () => {
+        describe('Empty, null or undefined values', () => {
+            const testPortNumber = 8082;
+            const incompleteRequest = '/route-weather?start=&end=&mode=&increment=';
+            startServer(testPortNumber);
+            it ('should reject with a 406 mentioning all the keys with invalid values', (done) => {
+                Chai.request(testUrl + testPortNumber)
+                .get(incompleteRequest)
+                .end((err, res) => {
+                    res.should.have.status(406);
+                    res.text.should.eql('Missing values in start, end, mode, increment');
+                    done();
+                })
+            })
         })
     })
 })
